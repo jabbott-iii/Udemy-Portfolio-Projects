540470c11e3dd8de24574554dc48a72286f7792b
Disttack <josephabbottiii@gmail.com>
2025-12-28 18:14:48 -0700

    created easy var for var management as module

---
+++ easy-var.py
@@ -0,0 +1,6 @@
+# A module for easy variable management.
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+letters = list(alphabet)
+symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', "'", '"', ',', '.', '<', '>', '/', '?', ' ']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
