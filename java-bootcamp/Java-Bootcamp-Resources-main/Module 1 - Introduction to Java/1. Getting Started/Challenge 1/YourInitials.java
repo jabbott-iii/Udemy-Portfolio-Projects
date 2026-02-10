d2b4ef3e101de3c46934f72d7106621e234d16ae
jabbott-iii <code.jabbott.iii@gmail.com>
2026-02-09 23:27:16 -0700

    started course

--- java-bootcamp/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ java-bootcamp/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,14 @@
 public class YourInitials {
     public static void main(String[] args) {
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+        System.out.println("""
+                JJJJJ    A
+                  J     A A
+                  J    A   A
+                  J    AAAAA
+                  J    A   A
+                J J    A   A
+                 JJ    A   A
+                """);
     }
 }
---
+++ java-bootcamp/section2/src/Variable.java
@@ -0,0 +1,8 @@
+public class Variable {
+    public static void main(String[] args) {
+        int passengers = 5;
+        System.out.println(passengers);
+
+        int busTickets = passengers;
+    }
+}
